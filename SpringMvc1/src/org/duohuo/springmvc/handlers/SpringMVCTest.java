233094a4e74bbecbbc0cf3459893bd74c635188a
KwSun <1023519351@qq.com>
2015-06-21 21:59:01 +0800

    add SpringMVCTest.java

---
+++ SpringMvc1/src/org/duohuo/springmvc/handlers/SpringMVCTest.java
@@ -0,0 +1,22 @@
+package org.duohuo.springmvc.handlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/springmvc")
+@Controller
+public class SpringMVCTest {
+	private static final String SUCCESS = "success";
+	
+	/**
+	 * 使用Method属性来制定请求方式
+	 * @return
+	 */
+	@RequestMapping(value="/testMethod",method=RequestMethod.POST)
+	public String testMethod(){
+		System.out.println("testMethod");
+		return SUCCESS;
+	}
+	
+}
