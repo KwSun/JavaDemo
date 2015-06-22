26f6153417af25a95e8575317d5de68e1c4e6f36
KwSun <1023519351@qq.com>
2015-06-22 11:10:03 +0800

    add @testRequestParam

--- SpringMvc1/src/org/duohuo/springmvc/handlers/SpringMVCTest.java
+++ SpringMvc1/src/org/duohuo/springmvc/handlers/SpringMVCTest.java
@@ -4,11 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/springmvc")
 @Controller
 public class SpringMVCTest {
 	private static final String SUCCESS = "success";
+	
+	/**
+	 * @RequestParam来映射请求参数
+	 * value 请求参数的参数名
+	 * required 该参数是否必须，默认为true
+	 * defaultValue 请求参数的默认值
+	 * @param username
+	 * @param age
+	 * @return
+	 */
+	@RequestMapping(value="/testRequestParam")
+	public String testRequestParam(@RequestParam(value="username") String username,
+			@RequestParam(value="age", required=false,defaultValue="0") int age){
+		System.out.println("testRequestParam,username:"+username+",age:"+age);
+		return SUCCESS;
+	}
 	
 	/**
 	 * 使用Method属性来制定请求方式
