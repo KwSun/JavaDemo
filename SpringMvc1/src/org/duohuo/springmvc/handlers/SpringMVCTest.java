317bfc6634b641126777f77d7bc2b55c74f6ab97
KwSun <1023519351@qq.com>
2015-06-25 20:33:07 +0800

    add entities,alter SpringMVCTest.java

--- SpringMvc1/src/org/duohuo/springmvc/handlers/SpringMVCTest.java
+++ SpringMvc1/src/org/duohuo/springmvc/handlers/SpringMVCTest.java
@@ -1,15 +1,61 @@
 package org.duohuo.springmvc.handlers;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Map;
+
+import org.duohuo.springmvc.entities.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
+@SessionAttributes(value={"user"})
 @RequestMapping("/springmvc")
 @Controller
 public class SpringMVCTest {
 	private static final String SUCCESS = "success";
+	
+	@RequestMapping("/testSessionAttributes")
+	public String testSessionAttributes(Map<String, Object> map) {
+		User user = new User("kw","123","1023519351@qq.com",22);
+		map.put("user",user);
+		return SUCCESS;
+	}
+	@RequestMapping("/testMap")
+	public String testMap(Map<String, Object> map) {
+		map.put("names", Arrays.asList("kw","lwq"));
+		return SUCCESS;
+	}
+	
+	@RequestMapping("/testModelAndView")
+	public ModelAndView testModelAndView() {
+		String viewName = SUCCESS;
+		ModelAndView modelAndView = new ModelAndView(viewName);
+		//添加模型数据到ModelAndView中
+		modelAndView.addObject("time", new Date());
+		return modelAndView;
+	}
+	
+	/**
+	 * Spring MVC 会按请求参数名和 POJO 属性名进行自动匹配， 自动为该对象填充属性值。支持级联属性。
+	 * 如：dept.deptId、dept.address.tel 等
+	 */
+	@RequestMapping("/testPojo")
+	public String testPojo(User user) {
+		System.out.println("testPojo: " + user);
+		return SUCCESS;
+	}
+	
+	@RequestMapping(value="/testCookieValue")
+	public String testCookieValue(@CookieValue("JSESSIONID") String sessionID){
+		System.out.println("testCookieValue:"+sessionID);
+		return SUCCESS;
+	}
 	
 	/**
 	 * @RequestParam来映射请求参数
